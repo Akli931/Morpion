763b5e37e9064b0d3eafd15089fbfa495e7f66a9
Akli931 <aklihadef2001@gmail.com>
2025-12-11 13:29:25 +0100

    reafctoring

---
+++ Morpion/Game.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Morpion
+{
+    public class Game
+    {
+        private Board board;
+        private Player playerX;
+        private Player playerO;
+        private Player currentPlayer;
+
+        public Game()
+        {
+            board = new Board();
+            playerX = new Player('X');
+            playerO = new Player('O');
+
+            currentPlayer = playerX; 
+        }
+
+
+        public void Start()
+        {
+            while (true)
+            {
+                
+                board.DisplayBoard();
+
+                Console.WriteLine();
+                Console.WriteLine($"Au tour du joueur {currentPlayer.Symbol}");
+
+                
+                var (row, col) = currentPlayer.GetNextMove(board);
+
+                
+                board.PlayMove(row, col, currentPlayer.Symbol);
+
+               
+                if (board.IsGameWon(currentPlayer.Symbol))
+                {
+                    board.DisplayBoard();
+                    Console.WriteLine();
+                    Console.WriteLine($" Le joueur {currentPlayer.Symbol} a gagné !");
+                    break;
+                }
+
+                
+                if (board.IsDraw())
+                {
+                    board.DisplayBoard();
+                    Console.WriteLine();
+                    Console.WriteLine("Match nul !");
+                    break;
+                }
+
+                
+                currentPlayer = (currentPlayer == playerX) ? playerO : playerX;
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Fin de la partie.");
+        }
+
+
+    }
+}
---
+++ Morpion/Player.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Morpion
+{
+    public class Player
+    {
+        public char Symbol { get; }
+
+        public Player(char symbol)
+        { this.Symbol = symbol; }
+
+        public (int row, int col) GetNextMove(Board board)
+        {
+            int row;
+            int col;
+
+            while (true)
+            {
+                Console.Write($"Joueur {Symbol}, entre une ligne (0-2) : ");
+                bool okRow = int.TryParse(Console.ReadLine(), out row);
+
+                Console.Write($"Joueur {Symbol}, entre une colonne (0-2) : ");
+                bool okCol = int.TryParse(Console.ReadLine(), out col);
+
+                
+                if (!okRow || !okCol || row < 0 || row > 2 || col < 0 || col > 2)
+                {
+                    Console.WriteLine(" Saisie invalide, réessaie !");
+                    continue;
+                }
+
+                
+                if (!board.IsCellEmpty(row, col))
+                {
+                    Console.WriteLine(" Cette case est déjà occupée !");
+                    continue;
+                }
+
+                
+                return (row, col);
+            }
+        }
+
+    }
+}
