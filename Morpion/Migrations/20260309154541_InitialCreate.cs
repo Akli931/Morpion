c9d379595d01fef455a563c7b82e4c5a27d0ee7c
Akli931 <aklihadef2001@gmail.com>
2026-03-09 17:10:33 +0100

    add Database

---
+++ Morpion/Migrations/20260309154541_InitialCreate.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Morpion.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "games",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    played_at = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    game_mode = table.Column<string>(type: "text", nullable: false),
+                    result = table.Column<string>(type: "text", nullable: true),
+                    is_finished = table.Column<bool>(type: "boolean", nullable: false),
+                    board_state = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_games", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "games");
+        }
+    }
+}
